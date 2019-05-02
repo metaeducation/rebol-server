5dedf77438636c5126de76d3511fc78ac244b1ef
Giulio Lunati <giuliolunati@gmail.com>
2019-05-02 10:59:12 +0200

    build-apk.bash: add error check for single steps
    Also make build-assets a separated step

--- build-apk.bash
+++ build-apk.bash
@@ -38,8 +38,6 @@ done
 for n in aapt dx apksigner zipalign
 do check_tool $n; done
 
-echo "system ok."
-
 I_BOOTCLASSPATH="-I $ANDROID_JAR"
 OPT_BOOTCLASSPATH="-bootclasspath $ANDROID_JAR"
 
@@ -56,35 +54,33 @@ for i in assets bin gen obj res; do
 done
 rm -rf gen/* bin/* obj/*
 
+echo "SYSTEM OK."
+
 echo "MAKING R.java ..."
 aapt package -f -m -J ./gen \
     -M ./AndroidManifest.xml \
     -S ./res \
-    $I_BOOTCLASSPATH
+    $I_BOOTCLASSPATH \
+    || error "FAILED MAKING R.java"
 
 echo "COMPILING *.class ..."
 $JAVAC -d obj \
     `find java/ gen/ -name \*.java` \
     $OPT_BOOTCLASSPATH \
     $OPT_CLASSPATH \
-    -source 1.7 -target 1.7 # see above
+    -source 1.7 -target 1.7 \
+    || error "FAILED COMPILING *.class"
 
 echo "COMPILING classes.dex ..."
 dx --dex --output=./bin/classes.dex \
     ./obj \
-    $SPLIT_CLASSPATH
-    # ^--- right? wrong?
+    $SPLIT_CLASSPATH \
+    || error "FAILED COMPILING classes.dex"
 
 # If you have the error UNEXPECTED TOP-LEVEL EXCEPTION, 
 # it can be because you use old build tools
 # and DX try to translate java 1.7 rather than 1.8.
 # To solve the problem, you have to specify 1.7 java version in the previous javac command
-
-if [ -f ./build-assets.bash ]
-then
-    echo "BUILDING ASSETS ..."
-    bash ./build-assets.bash 
-fi
 
 echo "BUILDING unaligned.apk ..."
 aapt package -f -m \
@@ -92,25 +88,30 @@ aapt package -f -m \
     -M ./AndroidManifest.xml \
     -A ./assets \
     -S ./res \
-    $I_BOOTCLASSPATH
+    $I_BOOTCLASSPATH \
+    || error "BUILDING unaligned.apk ..."
 
 # "classes.dex" must be in current dir !!
 cd ./bin
-aapt add unaligned.apk classes.dex
+    aapt add unaligned.apk classes.dex \
+    || error "ADDING classes.dex TO unaligned.apk"
 cd ..
 
 case $HOME in
     /data/data/com.termux*)
         echo "ALIGNING+SIGNING unaligned.apk ..."
-        apksigner -p $PASSWORD $KEY ./bin/unaligned.apk ./bin/$NAME
+        apksigner -p $PASSWORD $KEY ./bin/unaligned.apk ./bin/$NAME \
+        || error "FAILED ALIGNING+SIGNING unaligned.apk ..."
     ;;
     *)
         echo "ALIGNING unaligned.apk ..."
         zipalign -f 4 \
             ./bin/unaligned.apk \
-            ./bin/$NAME
+            ./bin/$NAME \
+        || error "FAILED ALIGNING unaligned.apk ..."
         echo "SIGNING $NAME ..."
-        apksigner sign --ks $KEY ./bin/$NAME
+        apksigner sign --ks $KEY ./bin/$NAME \
+        || error "FAILED SIGNING $NAME ..."
     ;;
 esac
 
