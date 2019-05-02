d434e206c9ffd3101082a8871db151716870b58c
Giulio Lunati <giuliolunati@gmail.com>
2019-05-02 19:46:51 +0200

    Make replpad-js work offline

--- build-assets.bash
+++ build-assets.bash
@@ -1,5 +1,7 @@
 # CONFIG
 
+LOAD_R3_URL="https://metaeducation.s3.amazonaws.com/travis-builds/load-r3.js"
+
 function error { echo $1; exit 1; }
 
 [ $R3 ] || error "Please export R3=/path/to/r3-android-executable"
@@ -11,13 +13,17 @@ rm -rf assets
 mkdir assets
 mkdir -p assets-src/system
 
-#
+# BUILD
 
 cd assets-src
 
-zip -r0 ../assets/install.zip \
-  system/ \
-  replpad-js/
+zip -r0 ../assets/install.zip system/
+
+mv replpad-js/index.html .tmp
+grep $LOAD_R3_URL .tmp || error "Can't find $LOAD_R3_URL in replpad-js/index.html"
+sed "s@$LOAD_R3_URL@../system/load-r3.js@" .tmp > replpad-js/index.html
+zip -r0 ../assets/install.zip replpad-js/
+mv .tmp replpad-js/index.html
 
 cp install.sh ../assets
 cp $R3 ../assets
