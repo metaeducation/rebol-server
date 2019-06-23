ffe93c15ea966e16472d5b4a6ebce2341cad2fe9
Giulio Lunati <giuliolunati@gmail.com>
2019-06-23 18:53:50 +0200

    Change Server to foreground service
    We don't want the server be stopped in backgroud!

--- java/giuliolunati/rebolserver/Server.java
+++ java/giuliolunati/rebolserver/Server.java
@@ -1,19 +1,56 @@
 package giuliolunati.rebolserver;
 
-import android.app.Service;
+import giuliolunati.rebolserver.ForegroundService;
+import giuliolunati.rebolserver.NotificationIdFactory;
+
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Binder;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 import java.lang.ProcessBuilder;
 
-public class Server extends Service {
+public class Server extends ForegroundService {
 	private Process mProcess;
+  private static final int NOTIFICATION_ID = NotificationIdFactory.create();
+  private final IBinder mBinder;
+  private Notification.Builder mBuilder;
+  private PendingIntent mNotificationPendingIntent;
+
+  public class LocalBinder extends Binder {
+    public Server getService() {
+      return Server.this;
+    }
+  }
+
 	@Override
 	public IBinder onBind(Intent intent) {
-		return null;
+		return mBinder;
 	}
+
+  public Server() {
+    super(NOTIFICATION_ID);
+    mBinder = new LocalBinder();
+  }
+
+  @Override
+  protected Notification createNotification() {
+    Intent notificationIntent = new Intent(this, Server.class);
+    mNotificationPendingIntent = PendingIntent.getService(this, 0, notificationIntent, 0);
+    mBuilder = new Notification.Builder(this);
+    mBuilder.setSmallIcon(R.drawable.r3_icon);
+    mBuilder.setContentTitle("Rebol Server");
+    mBuilder.setTicker(null);
+    //mBuilder.setContentText("");
+    mBuilder.setContentIntent(mNotificationPendingIntent);
+    Notification mNotification = mBuilder.build();
+    mNotification.flags = Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
+    return mNotification;
+  }
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
