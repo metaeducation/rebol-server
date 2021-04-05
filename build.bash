5715896730becf8e4b305aeb45d843241f2bfad1
Brian Dickens <brian@hostilefork.com>
2021-04-04 23:09:40 -0400

    Make errors terminate script
    This changes to use `set -e` mode in the build.bash script.  This makes
    any errors more obvious since it will exit the script.
    
    (I had not cloned the repository with `--recurse-submodules` and so
    the httpd server folder was empty, but this did not stop the APK from
    being created.)

--- build.bash
+++ build.bash
@@ -1,11 +1,25 @@
-[ -e r3 ] \
-|| bash update-r3.bash
+# %build.bash
 
-[ -e assets-src/system/load-r3.js ] \
-|| bash update-libr3-js.bash
+# Make it so that errors exit the script (use
+# `source` instead of `bash` to inherit this
+# script's modes when running subscripts)
+#
+set -e
 
-bash build-assets.bash
+if [ ! -e r3 ]; then
+  echo "r3 binary not found"
+  echo "fetching prebuilt" 
+  source update-r3.bash
+fi
 
-bash build-apk.bash
+if [ ! -e assets-src/system/load-r3.js ]; then
+  echo "Wasm build not found in assets-src/system/"
+  echo "fetching Wasm build from Amazon S3" 
+  source update-libr3-js.bash
+fi
+
+source build-assets.bash
+
+source build-apk.bash
 
 # vim: set et sw=2:
